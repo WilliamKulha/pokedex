353723c1b2a755ae981d1c50b3b6003d770952b1
WilliamKulha <wkulha14@gmail.com>
2019-04-11 16:05:33 -0400

    Start styling the components.

--- src/components/body/Pokedex.jsx
+++ src/components/body/Pokedex.jsx
@@ -22,29 +22,4 @@ const Pokedex = (props) => (
 </div>
 )
 
-//   render() {
-//     let pokemons = this.state.pokedex.slice();
-//     if(pokemons.length > 0) {
-//       return (
-//         <div className="pokedex-wrapper">
-//         <div className="search-field">
-//           <input type="text" onChange={props.filterList}/>
-//         </div>
-//           {
-//             pokemons.filter(pokemon => 
-//                 pokemon.name.toLowerCase().includes(
-//                   this.props.search.toLowerCase())
-//             ).map((pokemon, index) =>
-//               <PokemonCard 
-//                 key={index}
-//                 pokemon={pokemon}
-//               />
-//               )
-//           }
-//         </div>
-//       );
-//     }   
-//   } 
-// };
-
 export default CSSModules(Pokedex, styles);
